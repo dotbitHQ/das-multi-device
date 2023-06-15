29e889a7645ef10d746bc52ad08f4a3742837abe
Jerry <i@sjis.me>
2023-06-16 04:20:57 +0800

    feat: update webrtc js

--- http_server/webrtc/index.js
+++ http_server/webrtc/index.js
@@ -93,6 +93,7 @@ function join() {
                         console.log("peers answer oniceconnectionstatechange", answerPc.iceConnectionState)
                         if (answerPc.iceConnectionState === "disconnected") {
                             const video = document.getElementById(peer.cid)
+                            console.log("video streamId: ", video.srcObject.id, "pc streamId: ", pcMap.get(peer.cid).streamId)
                             if (video.srcObject.id !== pcMap.get(peer.cid).streamId) {
                                 return
                             }
@@ -153,6 +154,7 @@ function join() {
                     console.log("offer answer oniceconnectionstatechange", answerPc.iceConnectionState)
                     if (answerPc.iceConnectionState === "disconnected") {
                         const video = document.getElementById(msg.from)
+                        console.log("video streamId: ", video.srcObject.id, "pc streamId: ", pcMap.get(msg.from).streamId)
                         if (video.srcObject.id !== pcMap.get(msg.from).streamId) {
                             return
                         }
