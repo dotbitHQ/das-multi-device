0bf86b2739879dbb5ea6721233d83964d561c129
michael <457813723@qq.com>
2023-07-25 14:49:58 +0800

    webauthn

--- dao/t_authorize.go
+++ dao/t_authorize.go
@@ -1,8 +1,12 @@
 package dao
 
-import "das-multi-device/tables"
+import (
+	"das-multi-device/tables"
+	"fmt"
+)
 
 func (d *DbDao) GetMasters(cid1 string) (authorize []*tables.TableAuthorize, err error) {
+	fmt.Println("cid1: ", cid1)
 	err = d.parserDb.Where("`slave_cid`=?", cid1).Find(&authorize).Error
 	return
 }
